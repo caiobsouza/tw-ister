28718c2f9fc9863273ddf45af0145b186a9e9b51
Caio Souza <caiobsouza@hotmail.com>
2018-06-28 16:26:48 -0300

    Refactoring test

--- test/twist.test.js
+++ test/twist.test.js
@@ -16,29 +16,18 @@ describe('Testing random twister', () => {
      expect(constants.colors).to.contains(result.color);
   });
 
-  it('should return a random colors', () => {
+  it('should return a random twisters', () => {
     let colisions = 0;
     for (let i = 0; i < 4; i++) {
         const firstResult = twist();
         const secondResult = twist();
         
-        if(firstResult.color === secondResult.color)
+        if((firstResult.part === secondResult.part) && (firstResult.color === secondResult.color))
             colisions++;
     }
     
     expect(colisions).to.be.lessThan(2);
  });
 
-  it('should return a random parts', () => {
-    let colisions = 0;
-    for (let i = 0; i < 4; i++) {
-        const firstResult = twist();
-        const secondResult = twist();
-        
-        if(firstResult.part === secondResult.part)
-            colisions++;
-    }
-    
-    expect(colisions).to.be.lessThan(2);
- });
+  
 })
